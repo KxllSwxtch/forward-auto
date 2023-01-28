1ad15453ff72c871f70ed6fd8e8a798d4767a49e
KxllSwxtch <dimashin9@gmail.com>
2023-01-28 16:25:38 -0500

    updated

--- src/components/SecondSection/SecondSection.js
+++ src/components/SecondSection/SecondSection.js
@@ -26,6 +26,73 @@ export default function SecondSection() {
 					minus ratione. Alias, quo.
 				</p>
 			</DescriptionBlock>
+			<div className='row'>
+				<div className='col'>
+					<Form className='rounded'>
+						<h1>Contact Us</h1>
+						<div class='mb-4'>
+							<label for='nameInput' class='form-label'>
+								Name
+							</label>
+							<input
+								type='text'
+								class='form-control'
+								id='nameInput'
+								aria-describedby='nameHelp'
+								required
+							/>
+						</div>
+						<div class='mb-4'>
+							<label for='phoneInput' class='form-label'>
+								Phone Number
+							</label>
+							<input
+								type='tel'
+								class='form-control'
+								id='phoneInput'
+								aria-describedby='phoneInput'
+								pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'
+								required
+							/>
+							<small>Format: 111-222-3333</small>
+						</div>
+						<div class='mb-4'>
+							<label for='vehicleYearInput' class='form-label'>
+								Vehicle Year
+							</label>
+							<input
+								type='number'
+								class='form-control'
+								id='vehicleYearInput'
+								min={1}
+								required
+							/>
+						</div>
+						<div class='mb-4'>
+							<label for='carModelInput' class='form-label'>
+								Car Model
+							</label>
+							<input
+								type='text'
+								class='form-control'
+								id='carModelInput'
+								required
+							/>
+						</div>
+						<div class='mb-4'>
+							<label for='carModelInput' class='form-label'>
+								Your Question
+							</label>
+							<textarea
+								type='text'
+								class='form-control'
+								id='carModelInput'
+								required
+							></textarea>
+						</div>
+					</Form>
+				</div>
+			</div>
 		</div>
 	)
 }
@@ -37,4 +104,13 @@ const DescriptionBlock = styled.div`
 	float: right;
 	margin-right: 50px;
 	margin-top: -250px;
+`
+
+const Form = styled.form`
+	width: 600px;
+	margin-top: 100px;
+	margin-right: 50px;
+	float: right;
+	background: #d7ff27;
+	padding: 40px;
 `
